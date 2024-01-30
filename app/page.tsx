71a6acca498752b51953f3fe349f0d626073d96b
HuyNQQ <518h0630@student.tdtu.edu.vn>
2024-01-30 23:13:58 +0700

    Fix localStorage error in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 import AdminPage from '@/app/admin/page';
 
 export default function Home() {
+  if (typeof window !== 'undefined') {
+    localStorage.getdata('data');
+  }
+
   return (
     <main>
       <AdminPage />
